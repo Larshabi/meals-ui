06ea053fb402243e7a1e1f0d329fa29aa4514779
Larshabi <lekanlasabi@gmail.com>
2023-02-10 18:26:45 +0530

    cart items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,30 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
+
 const Cart = (props)=>{
-const cartItems = (
-<ul className={classes['cart-items']}>
-    {[{id:'c1', name:'sushi', amount:2, price:12.99}].map(item=>(<li>{item.name}</li>))}
-</ul>) ;
+    const data = [{id:'c1', name:'sushi', amount:2, price:12.99}]
+// const cartItems = (
+// <ul className={classes['cart-items']}>
+//     {[{id:'c1', name:'sushi', amount:2, price:12.99}].map(( item)=>(<li>{item.name}</li>))}
+// </ul>) ;
     return (
-        <div>
-            {cartItems}
+        <Modal onClose={props.onClose}>
+            {/* {cartItems} */}
+            <ul className={classes['cart-items']}>
+                {data.map((item)=>(
+                    <li>{item.name}</li>
+                ))}
+            </ul>
             <div className={classes.total}>
-                <span>Total Amounts</span>
+                <span>Total Amount</span>
                 <span>36.52</span>
             </div>
-            <div></div>
-        </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
     )
 }
 
